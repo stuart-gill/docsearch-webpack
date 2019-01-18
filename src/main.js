921491e9c35f2dd63c48289b0f3931096ae89abd
Stuart Gill <stuart.a.gill@gmail.com>
2019-01-18 10:24:20 -0800

    build initial file structure and api calls

---
+++ src/main.js
@@ -0,0 +1,94 @@
+import { SearchDocs } from './search.js';
+import $ from 'jquery';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import './styles.css';
+import tvIcon from './images/oldTV.png';
+
+let tvElement = document.getElementById("tvicon");
+tvElement.src = tvIcon;
+
+$(document).ready(function () {
+
+  $("#submitButton").click(function (event) {
+    event.preventDefault();
+    $("#details").empty();
+    $("#name").empty();
+
+
+    let ailment = $('#ailment').val();
+    let newSearch = new SearchMovie();
+    let promise = newSearch.GetByAilment(ailment);
+
+    promise.then(function (response) {
+      let body = JSON.parse(response);
+      for (let i = 0; i < body.results.length; i++) {
+        $("#name").append(`<li id= ${body.results[i].id}>${body.results[i].name}</li>`);
+      }
+      // $("li").click(function (event) {
+      //   event.preventDefault();
+      //   $("#details").empty();
+      //   let id = this.getAttribute("id");
+
+      //   let detailsPromise = newSearch.GetDetails(id);
+      //   detailsPromise.then(function (response) {
+      //     let body = JSON.parse(response);
+
+
+
+      //     $("#details").append(`<img class='img-thumbnail' src='https://image.tmdb.org/t/p/original/${body.backdrop_path}' alt = 'pic'>`);
+      //     $("#details").append(`<h4>${body.name}</h4>`);
+      //     $("#details").append(`<p>Number of seasons: ${body.number_of_seasons}</p>`);
+      //     $("#details").append(`<p>Number of episodes: ${body.number_of_episodes}</p>`);
+      //     $("#details").append(`<p id='description'>Description: ${body.overview}</p>`);
+      //     $("#details").append(`<p>Episode runtime: ${body.episode_run_time[0]}</p>`);
+      //     $("#details").append(`<span>Stream on: ${body.networks[0].name}</span>`);
+
+      //     $("#details").fadeIn();
+
+      //   })
+
+      // })
+    })
+  })
+  // $("#randomButton").click(function (event) {
+  //   event.preventDefault();
+  //   $("#details").empty();
+  //   $("#name").empty();
+
+  //   let minutes = $('#minutes').val();
+  //   let genre = $('select#genre').val();
+  //   let newSearch = new SearchMovie(minutes, genre);
+  //   $("#submitButton").off();
+
+  //   let randomPromise = newSearch.GetRandom();
+
+  //   randomPromise.then(function (response) {
+  //     let body = JSON.parse(response);
+  //     console.log(body.results.length);
+  //     for (let i = 0; i < body.results.length; i++) {
+  //       $("#name").append(`<li class= ${body.results[i].id}>${body.results[i].name}</li>`);
+  //     }
+  //     $("li").click(function (event) {
+  //       $("#details").empty();
+
+  //       let id = this.getAttribute("class");
+
+  //       let detailsPromise = newSearch.GetDetails(id);
+  //       detailsPromise.then(function (response) {
+  //         let body = JSON.parse(response);
+
+
+  //         $("#details").append(`<img class='img-thumbnail' src='https://image.tmdb.org/t/p/original/${body.backdrop_path}' alt = 'pic'>`);
+  //         $("#details").append(`<h4>${body.name}</h4>`);
+  //         $("#details").append(`<p>Number of seasons: ${body.number_of_seasons}</p>`);
+  //         $("#details").append(`<p>Number of episodes: ${body.number_of_episodes}</p>`);
+  //         $("#details").append(`<p id='description'>Description: ${body.overview}</p>`);
+  //         $("#details").append(`<p>Episode runtime: ${body.episode_run_time[0]}</p>`);
+
+  //         $("#details").fadeIn();
+  //       })
+
+  //     })
+  //   })
+  // })
+})
---
+++ src/search.js
@@ -0,0 +1,54 @@
+import { API_KEY } from '../.env';
+export class SearchDocs {
+
+  
+  GetByName(name) {
+    return new Promise((resolve, reject) => {
+      let request = new XMLHttpRequest();
+      let url = `https://api.betterdoctor.com/2016-03-01/doctors?name=${name}&location=45.512%2C-122.658%2C100&user_location=45.512%2C-122.658&sort=distance-asc&skip=0&limit=15&user_key=${API_KEY}`;
+      request.onload = function () {
+        if (this.status === 200) {
+          resolve(request.response);
+        } else {
+          reject(Error(request.statusText));
+        }
+      }
+      request.open("GET", url, true);
+      request.send();
+    });
+  }
+
+
+  GetByAilment(ailment) {
+    return new Promise((resolve, reject) => {
+      let request = new XMLHttpRequest();
+      let url = `https://api.betterdoctor.com/2016-03-01/doctors?query=${ailment}&location=45.512%2C-122.658%2C100&user_location=45.512%2C-122.658&sort=distance-asc&skip=0&limit=15&user_key=${API_KEY}`;
+      request.onload = function () {
+        if (this.status === 200) {
+          resolve(request.response);
+        } else {
+          reject(Error(request.statusText));
+        }
+      }
+      request.open("GET", url, true);
+      request.send();
+    })
+  }
+}
+  // GetDetails(id) {
+  //   return new Promise((resolve, reject) => {
+  //     let request = new XMLHttpRequest();
+  //     let url = `https://api.themoviedb.org/3/tv/${id}?api_key=${API_KEY}&language=en-US`;
+  //     request.onload = function () {
+  //       if (this.status === 200) {
+  //         resolve(request.response);
+  //       } else {
+  //         reject(Error(request.statusText));
+  //       }
+  //     }
+  //     request.open("GET", url, true);
+  //     request.send();
+  //   });
+  // }
+
+
