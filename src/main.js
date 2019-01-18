96c317ba4ef8201b09ff4ac3870d704f30c669f5
Stuart Gill <stuart.a.gill@gmail.com>
2019-01-18 15:36:03 -0800

    all working, added message for no doctors returned

--- src/main.js
+++ src/main.js
@@ -9,24 +9,26 @@ docElement.src = doctors;
 
 $(document).ready(function () {
 
-  $("#submitButton").click(function (event) {
+  $("#ailmentButton").click(function (event) {
     event.preventDefault();
     $("#details").empty();
     $("#name").empty();
-
-
     let ailment = $('#ailment').val();
+    $("#docName").val('');
     let newSearch = new SearchDocs();
     let promise = newSearch.GetByAilment(ailment);
 
     promise.then(function (response) {
       let body = JSON.parse(response);
       console.log(body);
+      if (body.data.length===0){
+        $("#name").append('No doctors match your criteria');
+      }
       for (let i = 0; i < body.data.length; i++) {
         let docMiddleName = body.data[i].profile.middle_name;
-        if (docMiddleName===undefined){
+        if (docMiddleName === undefined) {
           docMiddleName = '';
-        } 
+        }
         $("#name").append(`<li id= ${i}> ${body.data[i].profile.first_name} ${docMiddleName} ${body.data[i].profile.last_name}</li>`);
       }
       $("li").click(function (event) {
@@ -35,13 +37,12 @@ $(document).ready(function () {
         let id = this.getAttribute("id");
         let acceptingNewPatients = "Not accepting new patients";
         let docMiddleName = body.data[id].profile.middle_name;
-        if (body.data[id].practices[0].accepts_new_patients === true){
-          acceptingNewPatients="Accepting new patients";
-        };
-
-        if (docMiddleName===undefined){
+        if (body.data[id].practices[0].accepts_new_patients === true) {
+          acceptingNewPatients = "Accepting new patients";
+        }
+        if (docMiddleName === undefined) {
           docMiddleName = '';
-        } 
+        }
         console.log(id);
         console.log(docMiddleName);
         $("#details").append(`<h4> ${body.data[id].profile.first_name} ${docMiddleName} ${body.data[id].profile.last_name}</h4>`);
@@ -49,58 +50,54 @@ $(document).ready(function () {
         $("#details").append(`<p>Phone Number: ${body.data[id].practices[0].phones[0].number}</p>`);
         $("#details").append(`<p>Address: ${body.data[id].practices[0].visit_address.street} ${body.data[id].practices[0].visit_address.city}, ${body.data[id].practices[0].visit_address.state} ${body.data[id].practices[0].visit_address.zip}</p>`);
         $("#details").append(`<p>${acceptingNewPatients}</p>`);
-        
-        // $("#details").append(`<p>Number of episodes: ${body.number_of_episodes}</p>`);
-        // $("#details").append(`<p id='description'>Description: ${body.overview}</p>`);
-        // $("#details").append(`<p>Episode runtime: ${body.episode_run_time[0]}</p>`);
-        // $("#details").append(`<span>Stream on: ${body.networks[0].name}</span>`);
-
         $("#details").fadeIn();
+      });
 
+      $("#docButton").click(function (event) {
+        event.preventDefault();
+        $("#details").empty();
+        $("#name").empty();
+        let docName = $('#docName').val();
+        $("#ailment").val('');
+        let newSearch = new SearchDocs();
+        let promise = newSearch.GetByDocName(docName);
+        promise.then(function (response) {
+          let body = JSON.parse(response);
+          console.log(body);
+          if (body.data.length===0){
+            $("#name").append('No doctors match your criteria, please try again!');
+          }
+          for (let i = 0; i < body.data.length; i++) {
+            let docMiddleName = body.data[i].profile.middle_name;
+            if (docMiddleName === undefined) {
+              docMiddleName = '';
+            }
+            $("#name").append(`<li id= ${i}> ${body.data[i].profile.first_name} ${docMiddleName} ${body.data[i].profile.last_name}</li>`);
+          }
+          $("li").click(function (event) {
+            event.preventDefault();
+            $("#details").empty();
+            let id = this.getAttribute("id");
+            let acceptingNewPatients = "Not accepting new patients";
+            let docMiddleName = body.data[id].profile.middle_name;
+            if (body.data[id].practices[0].accepts_new_patients === true) {
+              acceptingNewPatients = "Accepting new patients";
+            }
+
+            if (docMiddleName === undefined) {
+              docMiddleName = '';
+            }
+            console.log(id);
+            console.log(docMiddleName);
+            $("#details").append(`<h4> ${body.data[id].profile.first_name} ${docMiddleName} ${body.data[id].profile.last_name}</h4>`);
+            $("#details").append(`<img class='img-thumbnail' src='${body.data[id].profile.image_url}' alt = 'pic'>`);
+            $("#details").append(`<p>Phone Number: ${body.data[id].practices[0].phones[0].number}</p>`);
+            $("#details").append(`<p>Address: ${body.data[id].practices[0].visit_address.street} ${body.data[id].practices[0].visit_address.city}, ${body.data[id].practices[0].visit_address.state} ${body.data[id].practices[0].visit_address.zip}</p>`);
+            $("#details").append(`<p>${acceptingNewPatients}</p>`);
+            $("#details").fadeIn();
+          });
         })
-
       })
     })
   })
-  // $("#randomButton").click(function (event) {
-  //   event.preventDefault();
-  //   $("#details").empty();
-  //   $("#name").empty();
-
-  //   let minutes = $('#minutes').val();
-  //   let genre = $('select#genre').val();
-  //   let newSearch = new SearchMovie(minutes, genre);
-  //   $("#submitButton").off();
-
-  //   let randomPromise = newSearch.GetRandom();
-
-  //   randomPromise.then(function (response) {
-  //     let body = JSON.parse(response);
-  //     console.log(body.results.length);
-  //     for (let i = 0; i < body.results.length; i++) {
-  //       $("#name").append(`<li class= ${body.results[i].id}>${body.results[i].name}</li>`);
-  //     }
-  //     $("li").click(function (event) {
-  //       $("#details").empty();
-
-  //       let id = this.getAttribute("class");
-
-  //       let detailsPromise = newSearch.GetDetails(id);
-  //       detailsPromise.then(function (response) {
-  //         let body = JSON.parse(response);
-
-
-  //         $("#details").append(`<img class='img-thumbnail' src='https://image.tmdb.org/t/p/original/${body.backdrop_path}' alt = 'pic'>`);
-  //         $("#details").append(`<h4>${body.name}</h4>`);
-  //         $("#details").append(`<p>Number of seasons: ${body.number_of_seasons}</p>`);
-  //         $("#details").append(`<p>Number of episodes: ${body.number_of_episodes}</p>`);
-  //         $("#details").append(`<p id='description'>Description: ${body.overview}</p>`);
-  //         $("#details").append(`<p>Episode runtime: ${body.episode_run_time[0]}</p>`);
-
-  //         $("#details").fadeIn();
-  //       })
-
-  //     })
-  //   })
-  // })
-
+})
--- src/search.js
+++ src/search.js
@@ -3,7 +3,7 @@ var apiKey = process.env.apiKey;
 export class SearchDocs {
 
   
-  GetByName(name) {
+  GetByDocName(name) {
     return new Promise((resolve, reject) => {
       let request = new XMLHttpRequest();
       let url = `https://api.betterdoctor.com/2016-03-01/doctors?name=${name}&location=45.512%2C-122.658%2C100&user_location=45.512%2C-122.658&sort=distance-asc&skip=0&limit=15&user_key=${apiKey}`;
@@ -13,7 +13,7 @@ export class SearchDocs {
         } else {
           reject(Error(request.statusText));
         }
-      }
+      };
       request.open("GET", url, true);
       request.send();
     });
@@ -30,10 +30,10 @@ export class SearchDocs {
         } else {
           reject(Error(request.statusText));
         }
-      }
+      };
       request.open("GET", url, true);
       request.send();
-    })
+    });
   }
 }
   // GetDetails(id) {
